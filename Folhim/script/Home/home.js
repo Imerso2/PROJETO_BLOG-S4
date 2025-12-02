c1a2710db8ad1b2170242a013b1c64deef16e506
Renan <renancorreia730@gmail.com>
2025-12-01 23:18:05 -0300

    js maldito, se exploda

--- Folhim/script/Home/home.js
+++ Folhim/script/Home/home.js
@@ -6,11 +6,13 @@ const date = new Date();
 let usuarios = [];
 let times;
 
-const card = document.createElement("div");
+
 const container = document.getElementById("posts");
 const containerTop = document.getElementById("destaqueContainer");
+const containerPopulares = document.getElementById("populares");
 const searchInput = document.getElementById("searchInput");
-
+const postExentended = document.getElementById("postExentended");
+const postExentendedBox = document.getElementById("postExentendedBox");
 // coletar o filter
 searchInput.addEventListener("input", (e) => {
   const filter = e.target.value.trim().toLowerCase();
@@ -22,12 +24,10 @@ searchInput.addEventListener("input", (e) => {
 async function faztudo() {
    try {
     const res = await fetch(API_URL); 
-    usuarios = await res.json();
-    let a;
-    usuarios.for(a = 0; index < array.length; index++){)
-      const populares = array[index];
-      listarPopulares();
-    }
+    usuarios = await res.json()
+   
+    listarPopulares(usuarios.slice(1, 4));
+    console.log(usuarios.slice(1,4))
     listarArtigos();
     listarPopular(usuarios[0]);
     
@@ -58,14 +58,14 @@ async function listarArtigos(filter = "") {
           `
           <i class="nada text"></i>
           <h3>Nenhum post encontrado</h3>
-          <p>Clique em "Novo Usuário" para começar.</p>
+         
           `;
           container.appendChild(nada);
           return;
     }
     nada.innerHTML = "";
     container.innerHTML = "";
-    usuarios.forEach((u) => {
+    filtrados.forEach((u) => {
       const card = document.createElement("div");
       card.classList.add("postCard");
     
@@ -95,62 +95,93 @@ async function listarArtigos(filter = "") {
     console.error(err);
   }
 }
-
-async function listarPopular() {
+//lista o card maior o popular
+async function listarPopular(p) {
   try {
-    const res = await fetch(API_URL); 
-    usuarios = await res.json();
-
     const nada = document.createElement("div");
     nada.classList.add("nada");
 
-    if (usuarios.length === 0) {
+  if (usuarios.length === 0) {
           nada.innerHTML = 
           `
           <i class="nada text"></i>
           <h3>Nenhum post encontrado</h3>
-          <p>Clique em "Novo Usuário" para começar.</p>
+          
           `;
           containerTop.appendChild(nada);
           return;
     }
     nada.innerHTML = "";
     
-    usuarios.forEach((u) => {
+    
       const cardDestaque = document.createElement("div");
       cardDestaque.classList.add("card-destaque");
     
       // criando o card
       cardDestaque.innerHTML = 
       `
-                    <img src="${u.UrlImagem}" alt="" class="destaqueImg">
+                    <img src="${p.UrlImagem}" alt="" class="destaqueImg">
                     
                     <div class="info">
-                        <span class="tag">${u.categoria}</span>
+                        <span class="tag">${p.categoria}</span>
                         <div class="meta">
                         <img src="../../assets/calendário1.png" alt="" class="calendarioIcon">
-                          <span>${u.dataPost}</span>
+                          <span>${p.dataPost}</span>
                             <span>•</span>
                             <img src="../../assets/cronômetro.png" alt="" class="cronometroIcon">
-                            <span id="editadoAh"> Editado há tem que fazer a funcção</span>
+                            <span id="editadoAh">${tempoDesde(p.Date)}</span>
                         </div>
-                        <button  type="button" class="irArtigo"  onclick="location.href='BlogPage.html'">
-                            <h2 >O poder da inteligência artificial na análise de dados em grandes bases.</h2>
+                        <button type="button" class="irArtigo"  onclick="location.href='BlogPage.html'">
+                            <h2>${p.tituloPost}</h2>
                         </button>
                     </div>
       `
 
-                container.appendChild(card);
-                console.log("teste");
-                
-
-            });
-            
+                containerTop.appendChild(cardDestaque);
+               postExtendido(p)     
   } catch (err) {
-    console.error(err);
+    console.log(err);
   }
 }
+// lista os outros populares
+async function listarPopulares(p) {
+ 
+    const nada = document.createElement("div");
+    nada.classList.add("nada");
 
+    if (usuarios.length <=  1) {
+          nada.innerHTML = 
+          `
+          <i class="nada text"></i>
+          <h3>Nenhum post encontrado</h3>
+         
+          `;
+          containerPopulares.appendChild(nada);
+          return;
+    }
+    containerPopulares.innerHTML = "";
+      p.forEach((u) => {
+      const cardPopulares = document.createElement("div");
+      cardPopulares.classList.add("popular-item");
+    
+      // criando o card
+      cardPopulares.innerHTML = 
+      ` 
+      <img src="${u.UrlImagem}" alt="imagem">
+                    <div>
+                        <p class="tag">${u.categoria}</p>
+                    <button type="button"  class="irArtigo" onclick="location.href='BlogPage.html'">
+                        <h4 class="tituloLateral">${u.tituloPost}</h4>
+                    </button>
+                    <span class="meta" id="editadoAh">${tempoDesde(u.Date)}</span>
+                </div>
+      `
+      
+      containerPopulares.appendChild(cardPopulares);
+    });
+ 
+}
+// formatação do tempo
 function tempoDesde(times) {
 
     const agora = Date.now();
@@ -171,10 +202,38 @@ function tempoDesde(times) {
     const dias = Math.floor(horas / 24);
     return `editado há ${dias} dias`;
 }
-
+function postExtendido(p){
+  console.log("bosta")
+ postExentended.innerHTML =
+ ` 
+            <div class="image-area">
+                <img src="${p.UrlImagem}" alt="">
+            </div>
+            
+            <div class="post-card">
+                <span class="categoria">${p.categoria}</span>
+                <h1 class="text">"${p.tituloPost}"</h1>
+                
+                <div class="infor">
+                    <div class="meta">
+                        <img src="../../assets/calendário1.png" alt="" class="calendarioIcon">
+                        <span>${p.dataPost}</span>
+                        <span>•</span>
+                        <img src="../../assets/cronômetro.png" alt="" class="cronometroIcon">
+                        <span>${tempoDesde(p.Date)}</span>
+                    </div>
+                </div>
+            </div>
+            </div>
+                <section class="assuntoText" >
+                    <p>${p.assuntoPost}</p>
+                </section>`
+}
+postExentendedBox.appendChild(postExentended);
 
 // chamada de função
-listarArtigos();
+faztudo();
 
 
-
+// postes individuais hhahaha
+postExtendido(usuarios[0]);
